a0c869338b753b7c6096c82ded0cf7263b45b6e8
unknown <dusdjeks@naver.com>
2021-11-04 20:42:45 +0900

    const and let

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//let은 변경할 수 있지만, constant는 변경할 수 없다. 
+//var은 옛날 방식이므로 사용을 지양하자. 
+
 const a=5;
 const b=2;
-const myName="hyunwoo"
+let myName="hyunwoo"
 
 console.log(a+b);
 console.log(a*b);
 console.log(a/b);
 console.log("hello "+myName);
+
+myName="hyunwoong"
+
+console.log("your new name is "+myName);
