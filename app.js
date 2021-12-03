72b17659dd0fb03437c45e2aa7dd2081ed880294
unknown <dusdjeks@naver.com>
2021-12-03 11:38:03 +0900

    events

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const loginForm=document.querySelector(".login-form");
 const loginInput=document.querySelector(".login-form input");
-const loginButton=document.querySelector(".login-form button");
 
-function handleLoginBtnClick(){
-    const username=loginInput.value;
-    console.log(username);
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click",handleLoginBtnClick);
+//함수 뒤에 onLoginSubmit()과 같이 작성하는 것은 함수를 바로 실행하길 원한
+//다는 것인데, 우리는 그것을 원하지 않기 때문에 이렇게 작성한다. 
+//우리가 이렇게 적어놓으면 loginForm에 이벤트가 발생했을 때, 이 함수가 
+//실행된다고 할 수 있다. 
+loginForm.addEventListener("submit",onLoginSubmit);
