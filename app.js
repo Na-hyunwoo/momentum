d3d27e80a6fb066063e2ba64c5947bf8c1397f7c
unknown <dusdjeks@naver.com>
2021-12-04 10:19:51 +0900

    event part two

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const loginForm=document.querySelector(".login-form");
 const loginInput=document.querySelector(".login-form input");
 
+const link=document.querySelector("a");
 
 function onLoginSubmit(event){
     event.preventDefault();
     console.log(loginInput.value);
+}
+
+function handleLinkClick(event){
+    event.preventDefault();
+    console.log(event);
 }
 
 //함수 뒤에 onLoginSubmit()과 같이 작성하는 것은 함수를 바로 실행하길 원한
@@ -12,3 +18,6 @@ function onLoginSubmit(event){
 //우리가 이렇게 적어놓으면 loginForm에 이벤트가 발생했을 때, 이 함수가 
 //실행된다고 할 수 있다. 
 loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
+
+handleLinkClick
