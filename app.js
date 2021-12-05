fe2bbb2d817c294b7b242f17a39eff546f37c35b
unknown <dusdjeks@naver.com>
2021-12-05 12:19:20 +0900

    Getting username

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const loginForm=document.querySelector(".login-form");
 const loginInput=document.querySelector(".login-form input");
+const greeting=document.querySelector(".greeting")
 
-const link=document.querySelector("a");
+const HIDDEN_CLASSNAME="hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    const username=loginInput.value;
+    loginForm.classList.add("hidden");
+    greeting.innerText=`안녕 ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event);
-}
+
 
 //함수 뒤에 onLoginSubmit()과 같이 작성하는 것은 함수를 바로 실행하길 원한
 //다는 것인데, 우리는 그것을 원하지 않기 때문에 이렇게 작성한다. 
 //우리가 이렇게 적어놓으면 loginForm에 이벤트가 발생했을 때, 이 함수가 
 //실행된다고 할 수 있다. 
 loginForm.addEventListener("submit",onLoginSubmit);
-link.addEventListener("click",handleLinkClick);
 
-handleLinkClick
+
