3d85e49cc4049295c75f27b06dea6998725e0c9a
unknown <dusdjeks@naver.com>
2021-12-13 10:14:38 +0900

    quotes

---
+++ js/quotes.js
@@ -0,0 +1,52 @@
+const quotes=[
+    {
+        quote: "1. The way to get started is to quit talking and begit doing",
+        author: "Walt Disney",
+    },
+    {
+        quote: "2. The way to get started is to quit talking and begit doing",
+        author: "Walt Disney",
+    },
+    {
+        quote: "3. The way to get started is to quit talking and begit doing",
+        author: "Walt Disney",
+    },
+    {
+        quote: "4. The way to get started is to quit talking and begit doing",
+        author: "Walt Disney",
+    },
+    {
+        quote: "5. The way to get started is to quit talking and begit doing",
+        author: "Walt Disney",
+    },
+    {
+        quote: "6. The way to get started is to quit talking and begit doing",
+        author: "Walt Disney",
+    },
+    {
+        quote: "7. The way to get started is to quit talking and begit doing",
+        author: "Walt Disney",
+    },
+    {
+        quote: "8. The way to get started is to quit talking and begit doing",
+        author: "Walt Disney",
+    },
+    {
+        quote: "9. The way to get started is to quit talking and begit doing",
+        author: "Walt Disney",
+    },
+    {
+        quote: "10. The way to get started is to quit talking and begit doing",
+        author: "Walt Disney",
+    }
+];
+const len=quotes.length;
+const random=Math.floor(Math.random()*len);
+
+const quote=document.querySelector("#quote span:first-child");
+const author=document.querySelector("#quote span:last-child");
+
+const todayQuote=quotes[random];
+
+quote.innerText=todayQuote.quote;
+author.innerText=todayQuote.author;
